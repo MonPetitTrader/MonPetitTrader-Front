5d57b1689e9026ca7fa54b1965d5c64db73f8d73
Quentin Jarry <quent1_jarry@hotmail.com>
2024-11-19 16:10:50 +0100

    create dropdown component

--- monpetittrader/src/app/app.component.ts
+++ monpetittrader/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DebugComponent } from './debug/debug.component';
 import { CandlestickChartComponent } from './components/commons/candlestick-chart/candlestick-chart.component';
+import { DropdownSelectorComponent } from "./components/commons/dropdown-selector/dropdown-selector.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DebugComponent, CandlestickChartComponent],
+  imports: [RouterOutlet, DebugComponent, CandlestickChartComponent, DropdownSelectorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ monpetittrader/src/app/components/commons/dropdown-selector/dropdown-selector.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown-selector',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dropdown-selector.component.html',
+  styleUrl: './dropdown-selector.component.css'
+})
+export class DropdownSelectorComponent {
+  symbols = ["BRK.A", "BRK.B", "TSM", "WMT", "JPM", "LLY", "V", "UNH", "XOM", "ORCL", "MA", "NVO", "PG", "HD", "JNJ", "BAC", "CRM", "ABBV", "CVX", "SAP", "KO", "MRK", "WFC", "TM", "BX", "ACN", "MS", "NOW", "MCD", "NVS", "SHEL", "DIS", "PM", "ABT", "BABA", "AXP", "IBM", "GS", "GE", "TMO", "CAT", "VZ", "RY", "DHR", "T", "HSBC", "BLK", "RTX", "HDB", "NEE", "SPGI", "PGR", "SYK", "LOW", "SCHW", "UBER", "UL", "ETN", "UNP", "PFE", "TTE", "MUFG", "PLTR", "KKR", "SHOP", "TJX", "BSX", "BHP", "COP", "C", "LMT", "FI", "ANET", "CB", "SONY", "BMY", "UPS", "BUD", "NKE", "MMC", "DE", "MDT", "BA", "PLD", "RIO", "IBN", "UBS", "TD", "SO", "MO", "APO", "DELL", "AMT", "SHW", "SMFG", "ELV", "ENB", "SPOT", "TT", "ICE"].sort()
+
+  @Output() selectionChange = new EventEmitter<String>();
+
+  onSelect(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    this.selectionChange.emit(value);
+    console.log(value) 
+  }
+}
